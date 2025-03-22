5a7f318e4a19ae5ea45d5336e010b46f7b031c7c
anuraginbytes <anuragpoddar9484@gmail.com>
2025-03-22 11:39:43 +0530

    fix component exports

---
+++ components/index.ts
@@ -0,0 +1,15 @@
+import TestimonialCard from "./TestimonialCard";
+import PricingCard from "./PricingCard";
+import FeatureCard from "./FeatureCard";
+import Navbar from "./NavBar";
+import Footer from "./Footer";
+
+
+
+export {
+  TestimonialCard,
+  PricingCard,
+  FeatureCard,
+  Navbar,
+  Footer,
+}
